86be198cdb4bdc7c051e2b517ddf38986532321b
Fofif <fofif_ii@hotmail.fr>
2013-06-29 20:26:17 +0800

    Added a context concept for variables + global vars
    - New assign management to take into account global vars
    - new WTemplate::pushContext() method
    - new WTemplate::popContext() method

--- WTemplate.php
+++ WTemplate.php
@@ -29,6 +29,11 @@ class WTemplate {
 	private $tpl_vars = array();
 	
 	/**
+	 * @var Set of variables contexts
+	 */
+	public $context = array();
+	
+	/**
 	 * @var WTemplateCompiler Template compilator
 	 */
 	private $compiler;
@@ -74,16 +79,61 @@ class WTemplate {
 	}
 	
 	/**
+	 * Changes the variables context in WTemplate
+	 * 
+	 * If context is changed, next parsed file's variables won't overwrite previous variables.
+	 */
+	public function pushContext() {
+		// Count the number of contexts created
+		$index = count($this->context);
+		
+		// If no context
+		if ($index == 0 && !empty($this->tpl_vars)) {
+			// Copy the variables in context #0
+			$this->context[0] = $this->tpl_vars;
+			$index = 1;
+		}
+		
+		// Create a new context
+		$this->context[$index] = array();
+		$this->tpl_vars = &$this->context[$index];
+	}
+	
+	/**
+	 * Come back to previous context
+	 */
+	public function popContext() {
+		// Count the number of contexts created
+		$index = count($this->context);
+		
+		if ($index >= 2) {
+			// Use context n-1 and clean the last context
+			$this->tpl_vars = &$this->context[$index-2];
+			unset($this->context[$index-1]);
+		} else if ($index == 1) {
+			// Actual context is #0, clean it
+			$this->context[0] = array();
+		}
+	}
+	
+	/**
 	 * Adds variables whose names are in names and their values to the private property $tpl_vars
 	 * 
 	 * @param array|string $names
 	 * @param array|mixed $values
 	 */
-	public function assign($names, $values = null) {
+	public function assign($names, $values = null, $global = false) {
 		if (is_array($names)) {
 			$this->tpl_vars = array_merge($this->tpl_vars, $names);
 		} else {
-			$this->tpl_vars[$names] = $values;
+			if ($global && count($this->context) != 0) { // if no context, use directly $tpl_vars
+				// For global vars, update value in all contexts
+				foreach ($this->context as $id => $vars) {
+					$this->context[$id][$names] = $values;
+				}
+			} else {
+				$this->tpl_vars[$names] = $values;
+			}
 		}
 	}
 	
